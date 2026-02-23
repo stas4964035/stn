ca45a2dcbd16aec3828dd8c965336b298460795d
stas4964035 <stas4964037@yandex.ru>
2026-02-23 10:06:18 +0300

    init

---
+++ src/main/java/com/k44/stn/StnApplication.java
@@ -0,0 +1,13 @@
+package com.k44.stn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/k44/stn/StnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k44.stn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
