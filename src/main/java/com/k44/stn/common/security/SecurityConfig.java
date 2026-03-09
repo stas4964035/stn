4c57c1b565e54ec91f8a00bc7c6a02fc2a34b26e
stas4964035 <65187168+stas4964035@users.noreply.github.com>
2026-03-09 14:42:34 +0300

    Remove HTTP Basic to force browser redirect to form login

--- src/main/java/com/k44/stn/common/security/SecurityConfig.java
+++ src/main/java/com/k44/stn/common/security/SecurityConfig.java
@@ -3,19 +3,23 @@ package com.k44.stn.common.security;
 import com.k44.stn.common.web.filter.RequestLoggingFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestHeaderRequestMatcher;
 
-import java.net.http.HttpClient;
+import java.util.Set;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public RequestLoggingFilter requestLoggingFilter(){
+    public RequestLoggingFilter requestLoggingFilter() {
         return new RequestLoggingFilter();
     }
 
@@ -25,22 +29,29 @@ public class SecurityConfig {
             RequestLoggingFilter requestLoggingFilter,
             RestAuthenticationEntryPoint restAuthenticationEntryPoint,
             RestAccessDeniedHandler restAccessDeniedHandler
-    ) throws Exception{
-        http.httpBasic(Customizer.withDefaults());
+    ) throws Exception {
+        http.formLogin(Customizer.withDefaults());
 
         http.authorizeHttpRequests(auth -> auth
                 .requestMatchers("/test").authenticated()
                 .anyRequest().permitAll()
         );
 
+        MediaTypeRequestMatcher jsonRequestMatcher = new MediaTypeRequestMatcher(MediaType.APPLICATION_JSON);
+        jsonRequestMatcher.setIgnoredMediaTypes(Set.of(MediaType.ALL));
+        RequestHeaderRequestMatcher ajaxRequestMatcher = new RequestHeaderRequestMatcher("X-Requested-With", "XMLHttpRequest");
+
         http.exceptionHandling(ex -> ex
-                .authenticationEntryPoint(restAuthenticationEntryPoint)
-                .accessDeniedHandler(restAccessDeniedHandler)
+                .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
+                .defaultAuthenticationEntryPointFor(restAuthenticationEntryPoint, jsonRequestMatcher)
+                .defaultAuthenticationEntryPointFor(restAuthenticationEntryPoint, ajaxRequestMatcher)
+                .defaultAccessDeniedHandlerFor(restAccessDeniedHandler, jsonRequestMatcher)
+                .defaultAccessDeniedHandlerFor(restAccessDeniedHandler, ajaxRequestMatcher)
         );
 
         http.csrf(AbstractHttpConfigurer::disable);
 
-        http.addFilterAfter(requestLoggingFilter, BasicAuthenticationFilter.class);
+        http.addFilterAfter(requestLoggingFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
